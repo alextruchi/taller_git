4bcdf4b3901b9f1742c39268820d3eb3ba6cadac
CristianCM17 <cristiandavidcardoso@gmail.com>
2023-03-20 15:59:36 -0600

    Agregue los test del password

---
+++ pass/src/main/java/comprobacion/PassComprobation.java
@@ -0,0 +1,24 @@
+package comprobacion;
+
+public class PassComprobation {
+
+    public enum NivelSecurity{
+        DEBIL, MEDIA, FUERTE
+    }
+
+    public static NivelSecurity  comprobacion(String pass){
+        if (pass.length()<8) {
+            return NivelSecurity.DEBIL;
+        }
+        if (pass.matches("[a-zA-z]+")) {
+            return NivelSecurity.DEBIL;
+        }
+        if (pass.matches("[a-zA-z0-9]+")){
+            return NivelSecurity.MEDIA;
+        }
+
+            return NivelSecurity.FUERTE;
+
+
+    }
+}
---
+++ pass/src/test/java/comprobacion/PassComprobationTest.java
@@ -0,0 +1,32 @@
+package comprobacion;
+
+import org.junit.Test;
+
+import static comprobacion.PassComprobation.NivelSecurity.*;
+import static org.junit.Assert.*;
+
+public class PassComprobationTest {
+
+    @Test
+    public void DEBIL_MENOR_A_8_CARACTERES() {
+
+        assertEquals(DEBIL,PassComprobation.comprobacion("1234567"));
+    }
+
+    @Test
+    public void DEBIL_SOLO_LETRAS() {
+
+        assertEquals(DEBIL,PassComprobation.comprobacion("abcdefgh"));
+    }
+
+    @Test
+    public void MEDIUM_LETRAS_NUMEROS() {
+
+        assertEquals(MEDIA,PassComprobation.comprobacion("abcd1234"));
+    }
+    @Test
+    public void FUERTE_LETRAS_NUMEROS_SIMBOLOS() {
+
+        assertEquals(FUERTE,PassComprobation.comprobacion("abcd1234!"));
+    }
+}
