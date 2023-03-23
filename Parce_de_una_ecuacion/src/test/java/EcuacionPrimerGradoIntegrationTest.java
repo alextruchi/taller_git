469daabd4673053a76ef708da60f127c3f07861d
alextruchi <70550732+alextruchi@users.noreply.github.com>
2023-03-23 11:20:43 -0600

    Add files via upload

---
+++ Parce_de_una_ecuacion/src/test/java/EcuacionPrimerGradoIntegrationTest.java
@@ -0,0 +1,41 @@
+import org.example.EcuacionPrimerGrado;
+import org.junit.jupiter.api.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class EcuacionPrimerGradoIntegrationTest {
+
+    EcuacionPrimerGrado ecuacion = new EcuacionPrimerGrado();
+
+    @Test
+    public void solucionaEcuacionConMenos() {
+
+        Double result = ecuacion.obtenerResultado("2x - 1 = 0");
+
+        Double valueExpected = 0.5;
+
+        assertEquals(valueExpected, result);
+    }
+
+    @Test
+    public void solucionaEcuacionConMas() {
+
+        Double result = ecuacion.obtenerResultado("2x + 1 = 0");
+
+        Double valueExpected = -0.5;
+
+        assertEquals(valueExpected, result);
+    }
+
+    @Test
+    public void solucionaEcuacionConParte3Mayor0() {
+
+        Double result = ecuacion.obtenerResultado("2x + 1 = 10");
+
+        Double valueExpected = 4.5;
+
+        assertEquals(valueExpected, result);
+    }
+
+
+}
